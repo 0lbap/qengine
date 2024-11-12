0c0286d61e4985b2a87e8ebdbb0ad5f08860e527
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-11-12 16:27:02 +0100

    chore: updated HexaStore and tests

--- src/main/java/qengine/storage/RDFHexaStore.java
+++ src/main/java/qengine/storage/RDFHexaStore.java
@@ -1,6 +1,7 @@
 package qengine.storage;
 
 import fr.boreal.model.logicalElements.api.*;
+import fr.boreal.model.logicalElements.impl.SubstitutionImpl;
 import org.apache.commons.lang3.NotImplementedException;
 import qengine.model.RDFAtom;
 import qengine.model.StarQuery;
@@ -17,15 +18,20 @@ import java.util.stream.Collectors;
 public class RDFHexaStore implements RDFStorage {
 
     private HashMap<Integer, Term> dict = new HashMap<>();
-
+    // TODO: refactor List of Lists into Map of Map of Set
     private List<List<Integer>> atomIndexesSPO = new ArrayList<>();
-
+    private List<List<Integer>> atomIndexesSOP = new ArrayList<>();
+    private List<List<Integer>> atomIndexesPSO = new ArrayList<>();
+    private List<List<Integer>> atomIndexesPOS = new ArrayList<>();
+    private List<List<Integer>> atomIndexesOSP = new ArrayList<>();
+    private List<List<Integer>> atomIndexesOPS = new ArrayList<>();
     private int dictIndex = 1;
+    private int size = 0;
 
     @Override
     public boolean add(RDFAtom atom) {
         var terms = List.of(atom.getTripleSubject(), atom.getTriplePredicate(), atom.getTripleObject());
-        List<Integer> indexes = new ArrayList<>(List.of(0, 0, 0));
+        Integer[] indexes = new Integer[3];
         for (int i = 0; i < 3; i++) {
             if (dict.containsValue(terms.get(i))) {
                 int finalI = i;
@@ -34,25 +40,74 @@ public class RDFHexaStore implements RDFStorage {
                         .findFirst()
                         .get()
                         .getKey();
-                indexes.set(i, index);
+                indexes[i] = index;
             } else {
                 dict.put(dictIndex, terms.get(i));
-                indexes.set(i, dictIndex);
+                indexes[i] = dictIndex;
                 dictIndex++;
             }
         }
-        atomIndexesSPO.add(indexes);
+
+        // Vérifie si l'atome est déjà enregistré
+        if (atomIndexesSPO.contains(List.of(indexes[0], indexes[1], indexes[2]))) {
+            return false;
+        }
+
+        size++;
+
+        // S = 0 | P = 1 | O = 2
+        atomIndexesSPO.add(List.of(indexes[0], indexes[1], indexes[2]));
+        atomIndexesSOP.add(List.of(indexes[0], indexes[2], indexes[1]));
+        atomIndexesPSO.add(List.of(indexes[1], indexes[0], indexes[2]));
+        atomIndexesPOS.add(List.of(indexes[1], indexes[2], indexes[0]));
+        atomIndexesOSP.add(List.of(indexes[2], indexes[0], indexes[1]));
+        atomIndexesOPS.add(List.of(indexes[2], indexes[1], indexes[0]));
         return true;
     }
 
     @Override
     public long size() {
-        return atomIndexesSPO.size();
+        return size;
     }
 
     @Override
     public Iterator<Substitution> match(RDFAtom atom) {
-        throw new NotImplementedException();
+        Term subject = atom.getTripleSubject();
+        Term predicate = atom.getTriplePredicate();
+        Term object = atom.getTripleObject();
+
+        MatchAtomCase matchAtomCase = getMatchAtomCase(subject, predicate, object);
+
+        Substitution substitution = new SubstitutionImpl();
+
+        switch (matchAtomCase) {
+            case CONST_CONST_CONST -> {
+                return null;
+            }
+            case CONST_CONST_VAR -> {
+                // TODO: use SPO
+            }
+            case CONST_VAR_CONST -> {
+                // TODO: use SOP or OSP
+            }
+            case CONST_VAR_VAR -> {
+                // TODO: use SPO or SOP
+            }
+            case VAR_CONST_CONST -> {
+                // TODO: use POS or OPS
+            }
+            case VAR_CONST_VAR -> {
+                // TODO: use PSO or POS
+            }
+            case VAR_VAR_CONST -> {
+                // TODO: use OPS or OSP
+            }
+            case VAR_VAR_VAR -> {
+                // TODO: use anything
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -67,7 +122,46 @@ public class RDFHexaStore implements RDFStorage {
 
     @Override
     public String toString() {
-        return "HexaStore\n" + "|_ Dictionary: " + dict.toString() + "\n|_ Atom indexes: " + atomIndexesSPO.toString();
+        return "HexaStore"
+                + "\n|_ Dictionary: " + dict.toString()
+                + "\n|_ Atom indexes (SPO): " + atomIndexesSPO.toString()
+                + "\n|_ Atom indexes (SOP): " + atomIndexesSOP.toString()
+                + "\n|_ Atom indexes (PSO): " + atomIndexesPSO.toString()
+                + "\n|_ Atom indexes (POS): " + atomIndexesPOS.toString()
+                + "\n|_ Atom indexes (OSP): " + atomIndexesOSP.toString()
+                + "\n|_ Atom indexes (OPS): " + atomIndexesOPS.toString();
+    }
+
+    private enum MatchAtomCase {
+        CONST_CONST_CONST,
+        CONST_CONST_VAR,
+        CONST_VAR_CONST,
+        CONST_VAR_VAR,
+        VAR_CONST_CONST,
+        VAR_CONST_VAR,
+        VAR_VAR_CONST,
+        VAR_VAR_VAR,
+    }
+
+    private MatchAtomCase getMatchAtomCase(Term subject, Term predicate, Term object) {
+        if (subject.isConstant() && predicate.isConstant() && object.isConstant()) {
+            return MatchAtomCase.CONST_CONST_CONST;
+        } else if (subject.isConstant() && predicate.isConstant() && object.isVariable()) {
+            return MatchAtomCase.CONST_CONST_VAR;
+        } else if (subject.isConstant() && predicate.isVariable() && object.isConstant()) {
+            return MatchAtomCase.CONST_VAR_CONST;
+        } else if (subject.isConstant() && predicate.isVariable() && object.isVariable()) {
+            return MatchAtomCase.CONST_VAR_VAR;
+        } else if (subject.isVariable() && predicate.isConstant() && object.isConstant()) {
+            return MatchAtomCase.VAR_CONST_CONST;
+        } else if (subject.isVariable() && predicate.isConstant() && object.isVariable()) {
+            return MatchAtomCase.VAR_CONST_VAR;
+        } else if (subject.isVariable() && predicate.isVariable() && object.isConstant()) {
+            return MatchAtomCase.VAR_VAR_CONST;
+        } else if (subject.isVariable() && predicate.isVariable() && object.isVariable()) {
+            return MatchAtomCase.VAR_VAR_VAR;
+        }
+        return null;
     }
 
 }
--- src/test/java/qengine/storage/RDFHexaStoreTest.java
+++ src/test/java/qengine/storage/RDFHexaStoreTest.java
@@ -5,12 +5,12 @@ import fr.boreal.model.logicalElements.factory.impl.SameObjectTermFactory;
 import fr.boreal.model.logicalElements.impl.SubstitutionImpl;
 import org.apache.commons.lang3.NotImplementedException;
 import qengine.model.RDFAtom;
-import qengine.storage.RDFHexaStore;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests unitaires pour la classe {@link RDFHexaStore}.
@@ -25,6 +25,7 @@ public class RDFHexaStoreTest {
     private static final Literal<String> OBJECT_3 = SameObjectTermFactory.instance().createOrGetLiteral("object3");
     private static final Variable VAR_X = SameObjectTermFactory.instance().createOrGetVariable("?x");
     private static final Variable VAR_Y = SameObjectTermFactory.instance().createOrGetVariable("?y");
+    private static final Variable VAR_Z = SameObjectTermFactory.instance().createOrGetVariable("?z");
 
 
     @Test
@@ -57,17 +58,39 @@ public class RDFHexaStoreTest {
 
     @Test
     public void testAddRDFAtom() {
-        throw new NotImplementedException();
+        RDFHexaStore store = new RDFHexaStore();
+        RDFAtom rdfAtom = new RDFAtom(SUBJECT_1, PREDICATE_1, OBJECT_1);
+
+        assertTrue(store.add(rdfAtom), "Le RDFAtom devrait être ajouté avec succès.");
+
+        Collection<Atom> atoms = store.getAtoms();
+        assertTrue(atoms.contains(rdfAtom), "La base devrait contenir le RDFAtom ajouté.");
     }
 
     @Test
     public void testAddDuplicateAtom() {
-        throw new NotImplementedException();
+        RDFHexaStore store = new RDFHexaStore();
+        RDFAtom rdfAtom = new RDFAtom(SUBJECT_1, PREDICATE_1, OBJECT_1);
+
+        store.add(rdfAtom);
+        long oldSize = store.size(); // Taille du HexaStore après l'ajout du premier atome
+        assertFalse(store.add(rdfAtom), "Le RDFAtom ne devrait pas être ajouté s'il est déjà présent.");
+
+        assertEquals(store.size(), oldSize, "La taille du HexaStore doit être la même après l'ajout d'un doublon.");
     }
 
     @Test
     public void testSize() {
-        throw new NotImplementedException();
+        RDFHexaStore store = new RDFHexaStore();
+        assertEquals(0, store.size(), "La taille d'un HexaStore vide doit être de 0.");
+
+        RDFAtom rdfAtom1 = new RDFAtom(SUBJECT_1, PREDICATE_1, OBJECT_1);
+        store.add(rdfAtom1);
+        assertEquals(1, store.size() , "Après l'ajout d'un atome, la taille du HexaStore doit être de 1.");
+
+        RDFAtom rdfAtom2 = new RDFAtom(SUBJECT_2, PREDICATE_2, OBJECT_2);
+        store.add(rdfAtom2);
+        assertEquals(2, store.size(), "Après l'ajout de deux atomes, la taille du HexaStore doit être de 2.");
     }
 
     @Test
@@ -77,7 +100,7 @@ public class RDFHexaStoreTest {
         store.add(new RDFAtom(SUBJECT_2, PREDICATE_1, OBJECT_2)); // RDFAtom(subject2, triple, object2)
         store.add(new RDFAtom(SUBJECT_1, PREDICATE_1, OBJECT_3)); // RDFAtom(subject1, triple, object3)
 
-        // Case 1
+        // CASE 1 | <x, y, ?z>
         RDFAtom matchingAtom = new RDFAtom(SUBJECT_1, PREDICATE_1, VAR_X); // RDFAtom(subject1, predicate1, X)
         Iterator<Substitution> matchedAtoms = store.match(matchingAtom);
         List<Substitution> matchedList = new ArrayList<>();
@@ -91,6 +114,111 @@ public class RDFHexaStoreTest {
         assertEquals(2, matchedList.size(), "There should be two matched RDFAtoms");
         assertTrue(matchedList.contains(secondResult), "Missing substitution: " + firstResult);
         assertTrue(matchedList.contains(secondResult), "Missing substitution: " + secondResult);
+
+        // CASE 2
+        // <x, ?y, z>
+
+        RDFAtom matchingAtom2 = new RDFAtom(SUBJECT_1, VAR_X, OBJECT_1);
+        Iterator<Substitution> matchedAtoms2 = store.match(matchingAtom2);
+        List<Substitution> matchedList2 = new ArrayList<>();
+        matchedAtoms2.forEachRemaining(matchedList2::add);
+
+        Substitution firstResult2 = new SubstitutionImpl();
+        firstResult2.add(VAR_X, PREDICATE_1);
+
+        Substitution secondResult2 = new SubstitutionImpl();
+        secondResult2.add(VAR_X, PREDICATE_2);
+
+        assertEquals(1, matchedList2.size(), "There should be one matched RDFAtom");
+        assertTrue(matchedList2.contains(firstResult2), "Missing substitution: " + firstResult2);
+        assertFalse(matchedList2.contains(secondResult2), "The atom doesn't exist: " + secondResult2);
+
+        // CASE 3 | <?x, y, z>
+        RDFAtom matchingAtom3 = new RDFAtom(VAR_X, PREDICATE_1, OBJECT_3);
+        Iterator<Substitution> matchedAtoms3 = store.match(matchingAtom3);
+        List<Substitution> matchedList3 = new ArrayList<>();
+        matchedAtoms3.forEachRemaining(matchedList3::add);
+
+        Substitution firstResult3 = new SubstitutionImpl();
+        firstResult3.add(VAR_X, SUBJECT_1);
+
+        Substitution secondResult3 = new SubstitutionImpl();
+        secondResult3.add(VAR_X, SUBJECT_2);
+
+        assertEquals(1, matchedList3.size(), "There should be one matched RDFAtom");
+        assertTrue(matchedList3.contains(firstResult3), "Missing substitution: " + firstResult3);
+        assertFalse(matchedList3.contains(secondResult3), "The atom doesn't exist: " + secondResult3);
+
+        // TODO Améliorer les tests unitaires suivants
+
+        // CASE 4 | <x, ?y, ?z>
+        RDFAtom matchingAtom4 = new RDFAtom(SUBJECT_1, VAR_Y, VAR_X);
+        Iterator<Substitution> matchedAtoms4 = store.match(matchingAtom4);
+        List<Substitution> matchedList4 = new ArrayList<>();
+        matchedAtoms4.forEachRemaining(matchedList4::add);
+
+        Substitution firstResult4 = new SubstitutionImpl();
+        firstResult4.add(VAR_Y, PREDICATE_1);
+        firstResult4.add(VAR_X, OBJECT_1);
+
+        assertEquals(2, matchedList4.size(), "There should be two matched RDFAtom");
+        assertTrue(matchedList4.contains(firstResult4), "Missing substitution: " + firstResult4);
+
+        // CASE 5 | <?x, ?y, z>
+        RDFAtom matchingAtom5 = new RDFAtom(VAR_X, VAR_Y, OBJECT_2);
+        Iterator<Substitution> matchedAtoms5 = store.match(matchingAtom5);
+        List<Substitution> matchedList5 = new ArrayList<>();
+        matchedAtoms5.forEachRemaining(matchedList5::add);
+
+        Substitution firstResult5 = new SubstitutionImpl();
+        firstResult5.add(VAR_X, SUBJECT_2);
+        firstResult5.add(VAR_Y, PREDICATE_1);
+
+        assertEquals(1, matchedList5.size(), "There should be one matched RDFAtom");
+        assertTrue(matchedList5.contains(firstResult5), "Missing substitution: " + firstResult5);
+
+
+        // CASE 6 | <?x, y, ?z>
+        RDFAtom matchingAtom6 = new RDFAtom(VAR_X, PREDICATE_1, VAR_Y);
+        Iterator<Substitution> matchedAtoms6 = store.match(matchingAtom6);
+        List<Substitution> matchedList6 = new ArrayList<>();
+        matchedAtoms6.forEachRemaining(matchedList6::add);
+
+        Substitution firstResult6 = new SubstitutionImpl();
+        firstResult6.add(VAR_X, SUBJECT_1);
+        firstResult6.add(VAR_Y, OBJECT_1);
+
+        assertEquals(3, matchedList6.size(), "There should be three matched RDFAtom");
+        assertTrue(matchedList6.contains(firstResult6), "Missing substitution: " + firstResult6);
+
+        // CASE 7 | <?x, ?y, ?z>
+        RDFAtom matchingAtom7 = new RDFAtom(VAR_X, VAR_Y, VAR_Z); // Toutes les composantes sont des variables
+        Iterator<Substitution> matchedAtoms7 = store.match(matchingAtom7);
+        List<Substitution> matchedList7 = new ArrayList<>();
+        matchedAtoms7.forEachRemaining(matchedList7::add);
+
+        // Création des substitutions attendues
+        Substitution result7_1 = new SubstitutionImpl();
+        result7_1.add(VAR_X, SUBJECT_1);
+        result7_1.add(VAR_Y, PREDICATE_1);
+        result7_1.add(VAR_Z, OBJECT_1);
+
+        Substitution result7_2 = new SubstitutionImpl();
+        result7_2.add(VAR_X, SUBJECT_2);
+        result7_2.add(VAR_Y, PREDICATE_1);
+        result7_2.add(VAR_Z, OBJECT_2);
+
+        Substitution result7_3 = new SubstitutionImpl();
+        result7_3.add(VAR_X, SUBJECT_1);
+        result7_3.add(VAR_Y, PREDICATE_1);
+        result7_3.add(VAR_Z, OBJECT_3);
+
+        // Vérification des résultats pour le cas 7
+        assertEquals(3, matchedList7.size(), "There should be three matched RDFAtoms");
+        assertTrue(matchedList7.contains(result7_1), "Missing substitution: " + result7_1);
+        assertTrue(matchedList7.contains(result7_2), "Missing substitution: " + result7_2);
+        assertTrue(matchedList7.contains(result7_3), "Missing substitution: " + result7_3);
+
 
         // Other cases
         throw new NotImplementedException("This test must be completed");
